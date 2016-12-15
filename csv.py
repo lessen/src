33c4844d87d5e213b2efc4ec33d7d7d163ba78ce
Tim Menzies <tim@menzies.us>
2016-12-15 18:32:02 -0500

    saving

--- csv.py
+++ csv.py
@@ -52,23 +52,26 @@ def csv(str=None, file=None, zip=None,
      yield row
 
 if __name__ == "__main__":
-  import time
+  import time,os
   stringOfData="""a,b,
   c,d
-  1,2,3,x
+  1,2.0,3,x
   10,20,30,y"""
   for row in csv(stringOfData, header=True):
     print(row)
-  for row in csv(file="data/weather.csv"):
-    print(row)
-  m,t1=1,time.time()
-  for row2 in csv(file="data/weatherLarge.csv"):
-    m += 1
-  n,t2=1,time.time()
-  for row1 in csv(file="weatherLarge.csv",
-                  zip="data/data.zip"):
-    n += 1
-  t3=time.time()
-  print(m,t2-t1,row1) # reading from raw ascii
-  print(n,t3-t2,row2) # reading from zip is slightly faster 
+  if os.path.isfile("data/weather.csv") and \
+     os.path.isfile("data/weatherLarge.csv") and\
+     os.path.isfile("data/data.zip"):
+    for row in csv(file="data/weather.csv"):
+      print(row)
+    m,t1=1,time.time()
+    for row2 in csv(file="data/weatherLarge.csv"):
+      m += 1
+    n,t2=1,time.time()
+    for row1 in csv(file="weatherLarge.csv",
+                    zip="data/data.zip"):
+      n += 1
+    t3=time.time()
+    print(m,t2-t1,row1) # reading from raw ascii
+    print(n,t3-t2,row2) # reading from zip is slightly faster 
   
