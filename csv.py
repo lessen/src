966cecc2bf23fa07655039eb49f447ccb7f0baa4
Tim Menzies <tim@menzies.us>
2016-12-15 18:27:38 -0500

    saving

--- csv.py
+++ csv.py
@@ -6,29 +6,26 @@ def csv(str=None, file=None, zip=None,
          header   = True,
          white    = '([\n\r\t]|#.*)'): 
  # ------------------------------
- def same(x,*_): return x
- def utf8(x)   : return x.decode("utf-8")
+ def same(x) : return x
+ def utf8(x) : return x.decode("utf-8")
  def compile(lst,rules):
-   def atom(z):
-     try:  return int(z),int
+   def isa(x):
+     try:  int(x); return int
      except ValueError:
-       try:  return float(z),float
+       try:  float(x); return float
        except ValueError:
-         return z, same
+         return same
    def comp1(i,x):
      if x != missing:
-       if rules[i] is None:
-         x,rule   = atom(x)
-         rules[i] = rule
-       return rules[i](x)
+       rule = rules[i] = rules[i] or isa(x)
+       return rule(x)
      return x
    return [comp1(i,cell) for i,cell in enumerate(lst)]
  # ------------------------------         
- def worker(src,n=0,rules=[], lines=[],filter=same):
+ def worker(src,n=0,rules=[], lines=[], filter=same):
    for line0 in src:
      line1 = filter(line0)
      line2 = re.sub(white, "", line1)
-     print("line",line2)
      if line2:
        lines += [line2]
        if line2[-1] != seperator:
@@ -45,7 +42,7 @@ def csv(str=None, file=None, zip=None,
    with zipfile.ZipFile(zip, 'r') as myzip:
      with myzip.open(file) as src:
        for row in worker(src,filter=utf8):
-         yield src
+         yield row
  elif file:
    with open(file) as src:
      for row in worker(src):
@@ -56,23 +53,22 @@ def csv(str=None, file=None, zip=None,
 
 if __name__ == "__main__":
   import time
-  data="""a,b,
+  stringOfData="""a,b,
   c,d
   1,2,3,x
   10,20,30,y"""
-  for row in csv(data,header=True):
+  for row in csv(stringOfData, header=True):
     print(row)
   for row in csv(file="data/weather.csv"):
     print(row)
-  with zipfile.ZipFile("data/data.zip") as z:
-    with z.open("weather.csv") as f:
-      for row in f:
-        print(row) #.encode('utf-8'))
-  for row in csv(file="weather.csv",zip="data/data.zip"):
-    print(1,row)
-#  t1 = time.time()
-#  n=1
-#  for row in csv(file="data/weatherLarge.csv"): n+= 1
-#  t2 = time.time()
-#  print(n,t2-t1,row)
-
+  m,t1=1,time.time()
+  for row2 in csv(file="data/weatherLarge.csv"):
+    m += 1
+  n,t2=1,time.time()
+  for row1 in csv(file="weatherLarge.csv",
+                  zip="data/data.zip"):
+    n += 1
+  t3=time.time()
+  print(m,t2-t1,row1) # reading from raw ascii
+  print(n,t3-t2,row2) # reading from zip is slightly faster 
+  
