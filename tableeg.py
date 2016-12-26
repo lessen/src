81c46eaed1ddca28ee7a1a33dbb3f3300189f209
Tim Menzies <tim@menzies.us>
2016-12-26 01:30:06 -0500

    saving

---
+++ tableeg.py
@@ -0,0 +1,10 @@
+from eg    import eg
+from table import table
+
+@eg
+def _table():
+  t=table(file="data/weather.csv")
+  for row in t.rows:
+    pass #print(row)
+
+if __name__ == "__main__" : eg()
